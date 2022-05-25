ac3d19497b879518b1743365e0a7d40e4e24c79e
Lucas Andrade <lucasilva108@gmail.com>
2022-05-24 23:15:31 -0300

    refactor: removing dogs controller

--- src/controllers/dogs.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller()
-export class DogsController {
-    @Get()
-    async getAll(): Promise<string> {
-        return 'returns all the dogs';
-    }
-}
