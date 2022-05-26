849ed2c7266434a6092710b657a2a97abac454bd
Lucas Andrade <lucasilva108@gmail.com>
2022-05-25 23:39:59 -0300

    chore: update cat

--- src/CartsModule/controllers/cats.controller.ts
+++ src/CartsModule/controllers/cats.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Param, Put } from "@nestjs/common";
 
 @Controller('cats')
 export class CatsController {
     @Get()
     async getAll(): Promise<string> {
         return 'return all cats from Cats Module';
+    }
+
+    @Put()
+    async update(@Param('id') id): Promise<string> {
+        return 'Update cat';
     }
 }
